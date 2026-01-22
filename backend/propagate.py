fb9eabc8fd90c4e8391a1ed2d397719204bc766d
ananyabhat145 <161270886+ananyabhat145@users.noreply.github.com>
2026-01-22 00:45:42 -0500

    Update propagate.py

--- backend/propagate.py
+++ backend/propagate.py
@@ -1,1 +1,24 @@
+import numpy as np
+from datetime import timedelta
+
+
+def propagate_satellite(satrec, start_time, minutes_ahead=1440, step=10):
+    """
+    Returns positions over time in km.
+    """
+    positions = []
+
+    for t in range(0, minutes_ahead, step):
+        future_time = start_time + timedelta(minutes=t)
+        jd, fr = satrec.jdsatepoch, satrec.jdsatepochF + (t / 1440.0)
+
+        e, r, v = satrec.sgp4(jd, fr)
+
+        if e == 0:
+            positions.append({
+                "t": t,
+                "pos": np.array(r)
+            })
+
+    return positions
 
